8c7bc6386219f302f539d8c12e3436b7a061631c
Nguyen Le Nam Anh <48341290+nguyenlenamanh@users.noreply.github.com>
2019-10-16 07:22:35 +0700

    first commit

---
+++ controllers/queryOthers.controllers.js
@@ -0,0 +1,116 @@
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "us-west-2",
+    accessKeyId: "accessKeyId",
+    secretAccessKey: "secretAccessKey",
+    endpoint: "http://localhost:8000"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var paramsCategories = {
+    TableName : "Others",
+    ProjectionExpression: "SortKey",
+    KeyConditionExpression: "PrimaryKey = :primaryKey",
+    ExpressionAttributeValues: {
+        ":primaryKey": "Category"
+    }
+};
+
+var paramsHot = {
+    TableName : "Others",
+    KeyConditionExpression: "PrimaryKey = :primaryKey",
+    ExpressionAttributeValues: {
+        ":primaryKey": "Hot"
+    }
+}
+/*
+
+// Danh sách category --> với từng category thì hiển thị ra tất cả sản phẩm thuộc category đó
+
+const promise = new Promise((resolve, reject) => {
+    docClient.query(paramsCategories, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            reject();
+        } else {
+            console.log("Query succeeded.");
+            return resolve(data.Items)
+        }
+    });
+})
+
+promise.then(function(categories) {
+
+    categories.forEach(cate => {
+        var paramsAllProductByCategory = {
+            TableName : "Products",
+            KeyConditionExpression: "CategoryName = :categoryName",
+            ExpressionAttributeValues: {
+                ":categoryName": cate.SortKey
+            }
+        }
+
+        docClient.query(paramsAllProductByCategory, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Query succeeded.");
+                console.log(data.Items);
+            }
+        });
+    })
+
+});*/
+
+
+
+// Danh sách sản phẩm hot --> với từng sản phẩm đó thì hiển thị ra tất cả thông tin của sản phẩm
+
+function getListProductID(){
+    return new Promise((resolve,reject) => {
+        docClient.query(paramsHot, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                reject();
+            } else {
+                return resolve(data.Items)
+            }
+        });
+    })
+}
+
+function asyncFunction(item) {
+    
+   return new Promise((resolve, reject) => {
+    var paramsProductID = {
+        TableName : "Products",
+        IndexName : "ProductIDIndex",
+        KeyConditionExpression: "ProductID = :productID",
+        ExpressionAttributeValues: {
+            ":productID": item.SortKey
+        }
+    };
+
+    docClient.query(paramsProductID, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            return err;
+        } else {
+            resolve(data.Items[0]);
+        }
+    });
+   })
+}
+
+module.exports.Index = async function Init(res) {
+    var array = await getListProductID();
+    
+    let promiseArray = array.map(asyncFunction);
+
+    Promise.all(promiseArray).then(result => {
+        res.render('index', { selected: 0, list_product: result });
+    });
+} 
+
---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var AWS = require("aws-sdk");
+
+var queryOthers = require('../controllers/queryOthers.controllers');
+
+AWS.config.update({
+  region: "us-west-2",
+  accessKeyId: "accessKeyId",
+  secretAccessKey: "secretAccessKey",
+  endpoint: "http://localhost:8000"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  queryOthers.Index(res);
+});
+
+
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { selected: 1 });
+});
+
+router.get('/product', function(req, res, next) {
+  res.render('product', { selected: 2 });
+});
+
+router.get('/cart', function(req, res, next) {
+  res.render('cart', { selected: 3 });
+});
+
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout', { selected: 4 });
+});
+
+module.exports = router;
